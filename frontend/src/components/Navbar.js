3d90f6444447b70215200fd6b95d7a22b544b736
Malidu Pahasara <malidupahasara04@gmail.com>
2024-09-07 17:09:26 +0530

    hotel name update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
       <nav className='navbar navbar-expand-lg navbar-light bg-primary p-2 text-white'>
         <div className='container-fluid text-white'>
           <a className='navbar-brand text-white' href='/'>
-            SHINE ON
+          STAY SUITE
           </a>
           <button
             className='navbar-toggler'
